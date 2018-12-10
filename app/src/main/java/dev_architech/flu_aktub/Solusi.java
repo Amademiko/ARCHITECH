7449b9c48f284e3af751a1033e1213a1a417b7ac
Amademiko <mikow45@gmail.com>
2018-12-10 01:56:03 -0500

    Assalamualaikum

---
+++ app/src/main/java/dev_architech/flu_aktub/Solusi.java
@@ -0,0 +1,13 @@
+package dev_architech.flu_aktub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Solusi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solusi);
+    }
+}
